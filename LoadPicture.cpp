71475d0ae33301fe3649947267c04c109e9c0e1d
Buiann05 <buikhanhan05@gmail.com>
2024-04-02 23:32:08 +0700

    refactor graphics module

--- LoadPicture.cpp
+++ LoadPicture.cpp
@@ -17,6 +17,10 @@ bool LoadPicture::loadImg(std::string path, SDL_Renderer* screen) {
     SDl_Surface* load_surface = IMG_Load(path.c_str());
     if (load_surface != nullptr) {
         SDL_SetColorKey(load_surface, SDL_TRUE, SDL_MapRGB(load_surface->format, COLOR_KEY_R, COLOR_KEY_G, COLOR_KEY_B));
-
+        new_texture = SDL_CreateTextureFromSurface(screen, load_surface);\
+        if (new_texture != nullptr) {
+            rect_.w=load_surface->w;
+            rect_.h=load_surface->h;
+        }
     }
 }
